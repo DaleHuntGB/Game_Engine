aedcfce102490d779c6c749c09ef2a0426794adf
Dale Hunt <dalehunt@gmail.com>
2023-03-27 17:18:32 +0100

    Initial Commit

---
+++ src/engine/custom/MyEngineSystem.cpp
@@ -0,0 +1,11 @@
+#include "MyEngineSystem.h"
+
+void DH_GUI::GUI_Initialize()
+{
+	printf("DH: GUI Initialized!\n");
+}
+
+void DH_GUI::GUI_DrawUI()
+{
+
+}
---
+++ src/engine/custom/MyEngineSystem.h
@@ -0,0 +1,21 @@
+#ifndef __MY_ENGINE_H__
+#define __MY_ENGINE_H__
+
+#include "../EngineCommon.h"
+
+class MyEngineSystem {
+	friend class XCube2Engine;
+	private:
+
+	public:
+};
+
+class DH_GUI {
+	private:
+
+	public:
+		void GUI_Initialize();
+		void GUI_DrawUI();
+};
+
+#endif
